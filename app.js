70edd99ac206807759b2accec2ef3e9eea0b8701
vjz3qz <vjz3qz@virginia.edu>
2023-03-12 15:47:44 -0400

    added for loop adder to ejs and added redirect

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
 //Author: Varun Pasupuleti
 
+//imports
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const https = require("https");
 
+//global variable
+var items = [];
+
+//server
 const app = express();
-
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.set('view engine', 'ejs');
 
+//initial get request
 app.get("/", (req, res) => {
     var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     var today  = new Date();
     const date = new Date();
-    res.render("list",{dayOfWeek:today.toLocaleDateString("en-US", options)});
+    res.render("list",{dayOfWeek:today.toLocaleDateString("en-US", options), itemList:items});
 });
 
+//post request
 app.post("/", (req,res) => {
-    console.log(req.body.newItem);
+    items.push(req.body.newItemtem)
+    res.redirect("/"); //redirects to get request with new item
 });
 
 app.listen(3000, () => {
