2c4aa39f39f85df44747b5dee5544a185c3bbc38
vjz3qz <vjz3qz@virginia.edu>
2023-03-12 15:14:03 -0400

    added content to list.ejs

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
+//Author: Varun Pasupuleti
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const https = require("https");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
-
 app.use('view engine', 'ejs');
 
-app.get("/", (req, res) => {
-    app.send("hello");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+    const date = new Date();
+    const numDay = date.getDay();
+    var day = "";
+    switch(numDay)
+    {
+        case 0:
+            day = "Sunday";
+        case 1:
+            day = "Monday";
+        case 2:
+            day = "Tuesday";
+        case 3:
+            day = "Wednesday";
+        case 4:
+            day = "Thursday";
+        case 5:
+            day = "Friday";
+        case 6:
+            day = "Saturday";
+    }
+    res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000, () => {
-    console.log("server running on port 3000");
+app.listen(3000, function() {
+    console.log("Server running on local host 3000");
 });
