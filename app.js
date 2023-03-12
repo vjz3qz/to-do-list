287292bce4b0a28c4e1ffb109a84650b9af048b5
vjz3qz <vjz3qz@virginia.edu>
2023-03-12 15:18:57 -0400

    removed index and added day of the week in app.js

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const https = require("https");
 
 const app = express();
 
-app.use('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", function(req, res) {
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
     const date = new Date();
     const numDay = date.getDay();
     var day = "";
@@ -19,22 +19,29 @@ app.get("/", function(req, res) {
     {
         case 0:
             day = "Sunday";
+            break;
         case 1:
             day = "Monday";
+            break;
         case 2:
             day = "Tuesday";
+            break;
         case 3:
             day = "Wednesday";
+            break;
         case 4:
             day = "Thursday";
+            break;
         case 5:
             day = "Friday";
+            break;
         case 6:
             day = "Saturday";
+            break;
     }
-    res.sendFile(__dirname + "/index.html");
+    res.render("list",{dayOfWeek:day});
 });
 
-app.listen(3000, function() {
-    console.log("Server running on local host 3000");
+app.listen(3000, () => {
+    console.log("server running on port 3000");
 });
